68f3f28dbf4d1bb8f63e433e678851fb8d618949
Galit <glukin@mit.edu>
2019-11-16 17:45:56 -0500

    putting ELemFunc in the right place

--- cs207-FinalProject/AD/ElemFunc.py
+++ cs207-FinalProject/AD/ElemFunc.py
@@ -1,7 +1,6 @@
 from AutoDiff import AutoDiff
 import math as math
 
-
 def sin(x):
     try:
         return AutoDiff(math.sin(x.val), math.cos(x.val)*x.der)
@@ -27,3 +26,51 @@ def log(x):
         return AutoDiff(math.log(x.val), (1/(x.val))*x.der)
     except AttributeError:
         return AutoDiff(math.log(x),0)
+
+def log10(x):
+    try:
+        return AutoDiff(math.log10(x.val), (1/(x.val))*x.der)
+    except AttributeError:
+        return AutoDiff(math.log10(x),0)
+
+def tan(x):
+    try:
+        return AutoDiff(math.tan(x.val), (2/(math.cos(x.val*2)+1))*x.der)
+    except AttributeError:
+        return AutoDiff(math.tan(x),0)
+
+def cot(x):
+    try:
+        return AutoDiff(math.cot(x.val), (2/(math.cos(x.val*2)-1))*x.der)
+    except AttributeError:
+        return AutoDiff(math.cot(x),0)
+
+def sec(x):
+    try:
+        return AutoDiff(1/(math.cos(x.val)), (math.tan(x.val)*1/(math.cos(x.val))*x.der))
+    except AttributeError:
+        return AutoDiff(1/math.cos(x),0)
+
+def csc(x):
+    try:
+        return AutoDiff(1/math.sin(x.val), (-1/math.sin(x.val)*math.cot(x.val))*x.der)
+    except AttributeError:
+        return AutoDiff(1/math.sin(x),0)
+
+def arcsin(x):
+    try:
+        return AutoDiff(math.asin(x.val), (1/math.sqrt(1-(x.val)*(x.val)))*x.der)
+    except AttributeError:
+        return AutoDiff(math.asin(x),0)
+
+def arccos(x):
+    try:
+        return AutoDiff(math.acos(x.val), (-1/math.sqrt(1-(x.val)*(x.val)))*x.der)
+    except AttributeError:
+        return AutoDiff(math.acos(x),0)
+
+def arctan(x):
+    try:
+        return AutoDiff(math.atan(x.val), (-1/(1+(x.val)*(x.val))*x.der))
+    except AttributeError:
+        return AutoDiff(math.atan(x),0)
