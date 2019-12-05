513d43db68245561360ccb7ec1b55733b6b1d410
Galit <glukin@mit.edu>
2019-12-05 16:05:25 -0500

    fixing paths in test

--- tests/test_MCMC.py
+++ tests/test_MCMC.py
@@ -1,9 +1,9 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+sys.path.append(os.path.join(os.path.dirname(__file__),'../AD'))
 
-import AD.ElemFunc as EF
-from AD.ADiff import ADiff
+import ElemFunc as EF
+from ADiff import ADiff
 sys.path.append(os.path.join(os.path.dirname(__file__),'../MCMC/'))
 from HMC import HMCSampler
 from MALA import MALASampler
