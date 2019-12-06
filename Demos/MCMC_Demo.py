35ac054055108a572894f061d62ef4ba9d709454
Galit <glukin@mit.edu>
2019-12-05 20:03:53 -0500

    chaning paths for distribution

--- Demos/MCMC_Demo.py
+++ Demos/MCMC_Demo.py
@@ -1,8 +1,8 @@
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__),'../AD'))
-import ElemFunc as EF
+import AD
+import AD.ElemFunc as EF
 
+import sys
+import os
 sys.path.append(os.path.join(os.path.dirname(__file__),'../MCMC'))
 from HMC import HMCSampler
 from MALA import MALASampler
