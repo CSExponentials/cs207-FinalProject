8a2888b9fb8320aa00c760561fe7388652367556
Galit <glukin@mit.edu>
2019-12-05 22:45:01 -0500

    change all of structure

--- AutoDiff_Exponentials/tests/test_AD.py
+++ AutoDiff_Exponentials/tests/test_AD.py
@@ -3,7 +3,14 @@
 # print(os.getcwd())
 # sys.path.append(os.path.join(os.path.dirname(__file__),'../AD/'))
 
-import ElemFunc as EF
+import os
+import sys
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
+sys.path.insert(0, PROJECT_DIR)
+
+from AD import ElemFunc as EF
 from AD import ADiff
 
 import pytest
